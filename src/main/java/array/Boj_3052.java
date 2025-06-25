4243b2c30f8818f9a5d96d4c7935b11203847b12
pyjhoop <pyjhoop@naver.com>
2025-06-25 20:20:46 +0900

    BOJ 3052
    문제
    총 10개의 숫자가 주어지고, 숫자들을 42로 나눈 나머지중 서로 다른 값을 갖는 값의 개수는?
    
    풀이
    42의 나머지는 0~41
    그래서 크기가 42인 배열을 생성하면 모두 값이 0으로 초기화됨.
    나머지값의 인덱스로 값이 해당 인덱스 값을 1씩 증가시킨뒤 0이 아닌 인덱스 개수를 출력

---
+++ src/main/java/array/Boj_3052.java
@@ -0,0 +1,29 @@
+package array;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj_3052 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[42];
+
+        int temp = 0;
+        for(int i = 0; i < 10; i++) {
+            temp = Integer.parseInt(br.readLine());
+
+            temp = temp % 42;
+            arr[temp]++;
+        }
+
+        int sum = 0;
+        for(int i : arr) {
+            if(i > 0) sum++;
+        }
+
+        System.out.println(sum);
+        br.close();
+    }
+}
