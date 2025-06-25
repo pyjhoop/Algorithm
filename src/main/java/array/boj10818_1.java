bfafd51ee59acdfe2dca990de4a24b0fb21d0923
pyjhoop <pyjhoop@naver.com>
2025-06-25 20:02:11 +0900

    BOJ 10818
    문제
    n과 n개의 숫자를 입력받는다.
    n개의 숫자중 최소값, 최대값을 찾아서 출력하면 된다.
    
    풀이
    1. 배열을 만들고 정렬시켜서 0번째 인덱스와 n-1번째 인덱스로 최소, 최대값 구함.
    2. 초기에 최소, 최대값 초기화시키고 값 하나 추출할때마다 비교해서 최소, 최대값 구함.

---
+++ src/main/java/array/boj10818_1.java
@@ -0,0 +1,30 @@
+package array;
+
+import java.util.*;
+import java.io.*;
+
+public class boj10818_1 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        String result = solution(n, arr);
+
+        System.out.println(result);
+    }
+
+    static String solution(int n, int[] arr){
+        StringBuilder sb = new StringBuilder();
+        Arrays.sort(arr);
+        sb.append(arr[0]).append(" ").append(arr[n-1]);
+        return sb.toString();
+    }
+}
---
+++ src/main/java/array/boj10818_2.java
@@ -0,0 +1,28 @@
+package array;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj10818_2 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        int minVal =Integer.MAX_VALUE;
+        int maxVal = Integer.MIN_VALUE;
+        for(int i = 0; i < n; i++) {
+            int a = Integer.parseInt(st.nextToken());
+            if(a < minVal) minVal = a;
+            if(a > maxVal) maxVal = a;
+        }
+        sb.append(minVal).append(" ").append(maxVal);
+
+        System.out.println(sb.toString());
+    }
+}
